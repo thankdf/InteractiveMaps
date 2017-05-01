83121521e93a3f57582906718ef5fd62c7c15faa
Sandeep Kaur <Sandeep@sandeeps-macbook-pro.local>
2017-05-01 01:29:28 -0700

    tab controller working fine

--- Skeye_UI_New/tabBarController.swift
+++ Skeye_UI_New/tabBarController.swift
@@ -2,16 +2,44 @@
 //  TabBarController.swift
 //  Skeye_UI_New
 //
-//  Created by Sandeep Kaur on 4/28/17.
+//  Created by Sandeep Kaur on 5/1/17.
 //  Copyright © 2017 Sandeep Kaur. All rights reserved.
 //
 
-import MapKit
 import UIKit
+import Foundation
 
-class tabBarController : UIViewController
-{
+class TabBarController : UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     
 }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let user_type = "attendee"
+        
+        if(user_type == "event coordinator")
+        {
+        viewControllers?.remove(at:1)
+        }
+        
+        else if(user_type == "booth owner")
+        {
+            viewControllers?.remove(at:2)
 
+        }
+        else if(user_type == "attendee")
+        {
+            viewControllers?.remove(at:1)
+            viewControllers?.remove(at:1)
 
+        }
+        
+        
+    }
+
+}
